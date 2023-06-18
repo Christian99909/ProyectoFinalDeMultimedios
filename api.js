d4a7fc96bc273b01db4278f98b291857ee91b47f
MoisesSegura <moisessegurarojas@gmail.com>
2023-06-18 01:34:08 -0600

    api artist y listado de albumes

--- api.js
+++ api.js
@@ -2,7 +2,9 @@
 $(document).ready(function() {
   var clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
   var clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
-  var apiUrl = 'https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb';
+  var apiUrl = 'https://api.spotify.com/v1/artists/1Yox196W7bzVNZI7RBaPnf?si=zF4HW-TCQhOm6M6jvJfzdw';
+
+  // https://open.spotify.com/artist/1Yox196W7bzVNZI7RBaPnf?si=zF4HW-TCQhOm6M6jvJfzdw
 
   // Realiza una solicitud para obtener el token de acceso
   $.ajax({
@@ -31,15 +33,79 @@ $(document).ready(function() {
 
           // Crea un contenedor para la información adicional
           var artistDetails = $('<div></div>');
-          artistDetails.append('<p class="seguidores">Seguidores: ' + response.followers.total + '</p>');
-          artistDetails.append('<p class="genre">Géneros: ' + response.genres.join(', ') + '</p>');
+          artistDetails.append('<p class="infoExt">Popularidad: ' + response.popularity + '</p>');
+          artistDetails.append('<p class="infoExt">Seguidores: ' + response.followers.total + '</p>');
+          artistDetails.append('<p class="infoExt">Géneros: ' + response.genres.join(', ') + '</p>');
 
-          // Agrega todo el contenido del artista (imagen, nombre y detalles) al div con el ID 'artist-info'
+          // se agrega todo el contenido del artista (imagen, nombre y detalles) al div info'
           $('#artist-info').html(artistInfo).append(artistDetails);
 
-          // Agrega el contenedor de la información adicional al div con el ID 'artist-details'
+          // se agrega el contenedor de la información adicional al div details'
           $('#artist-details').html(artistDetails);
         },
+        error: function(error) {
+          console.log('Error:', error);
+        }
+      });
+    },
+    error: function(error) {
+      console.log('Error:', error);
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  var clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
+  var clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
+  var artistId = '1Yox196W7bzVNZI7RBaPnf';
+
+  // Realiza una solicitud para obtener el token de acceso
+  $.ajax({
+    url: 'https://accounts.spotify.com/api/token',
+    type: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+      'grant_type': 'client_credentials'
+    },
+    success: function(tokenResponse) {
+      var accessToken = tokenResponse.access_token;
+
+      // Realiza la solicitud a la API de Spotify utilizando el token de acceso
+      $.ajax({
+        url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
+        type: 'GET',
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        },
+        success: function(response) {
+          if (response.hasOwnProperty('items')) {
+            var albums = response.items;
+            var albumsContainer = $('#albums-container');
+        
+            // Itera sobre los álbumes y muestra la información en el HTML
+            albums.forEach(function(album) {
+              var albumContainer = $('<div class="album-container"></div>');
+              var albumImage = $('<img class="imgAlbum" src="' + album.images[0].url + '">');
+              var albumInfo = $('<div class="album-info"></div>');
+            
+              albumInfo.append('<h3>' + album.name + '</h3>');
+              albumInfo.append('<p class="album-info-line">Total de canciones: ' + album.total_tracks + '</p>');
+              albumInfo.append('<p class="album-info-line">Fecha de lanzamiento: ' + album.release_date + '</p>');
+            
+              albumContainer.append(albumImage);
+              albumContainer.append(albumInfo);
+            
+              albumsContainer.append(albumContainer);
+            });
+          } else {
+            console.log('La respuesta de la API no contiene la propiedad "items".', response);
+          }
+        },
+        
         error: function(error) {
           console.log('Error:', error);
         }

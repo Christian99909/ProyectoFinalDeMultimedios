fb89979764ad7423dee69516f82de6289f5df4d0
MoisesSegura <moisessegurarojas@gmail.com>
2023-07-02 20:47:29 -0600

    se agrego 'mostrar album'

--- api.js
+++ api.js
@@ -1,119 +1,280 @@
-$(document).ready(function() {
+//INFORMACION DEL ARTISTA--------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
   var clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
   var clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
   var apiUrl = 'https://api.spotify.com/v1/artists/1Yox196W7bzVNZI7RBaPnf?si=zF4HW-TCQhOm6M6jvJfzdw';
 
+  // https://open.spotify.com/artist/1Yox196W7bzVNZI7RBaPnf?si=zF4HW-TCQhOm6M6jvJfzdw
+
   // Realiza una solicitud para obtener el token de acceso
-  $.ajax({
-    url: 'https://accounts.spotify.com/api/token',
-    type: 'POST',
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
     headers: {
       'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    data: {
-      'grant_type': 'client_credentials'
-    },
-    success: function(tokenResponse) {
-      var accessToken = tokenResponse.access_token;
-
-      // Realiza la solicitud a la API de Spotify utilizando el token de acceso
-      $.ajax({
-        url: apiUrl,
-        type: 'GET',
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        },
-        success: function(response) {
-          var artistInfo = '<img class="imgArtist" src="' + response.images[0].url + '" alt="Imagen del artista">';
-          artistInfo += '<h2 class="responseName">' + response.name + '</h2>';
-
-          // Crea un contenedor para la información adicional
-          var artistDetails = $('<div></div>');
-          artistDetails.append('<p class="infoExt">Popularidad: ' + response.popularity + '</p>');
-          artistDetails.append('<p class="infoExt">Seguidores: ' + response.followers.total + '</p>');
-          artistDetails.append('<p class="infoExt">Géneros: ' + response.genres.join(', ') + '</p>');
-
-          // se agrega todo el contenido del artista (imagen, nombre y detalles) al div info'
-          $('#artist-info').html(artistInfo).append(artistDetails);
-
-          // se agrega el contenedor de la información adicional al div details'
-          $('#artist-details').html(artistDetails);
-        },
-        error: function(error) {
-          console.log('Error:', error);
-        }
-      });
-    },
-    error: function(error) {
-      console.log('Error:', error);
+    body: 'grant_type=client_credentials'
+  })
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error al obtener el token de acceso');
     }
+  })
+  .then(function(tokenResponse) {
+    var accessToken = tokenResponse.access_token;
+
+    // Realiza la solicitud a la API de Spotify utilizando el token de acceso
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error al obtener la información del artista');
+      }
+    })
+    .then(function(response) {
+      var artistInfo = '<img class="imgArtist" src="' + response.images[0].url + '" alt="Imagen del artista">';
+      artistInfo += '<h2 class="responseName">' + response.name + '</h2>';
+
+      // Crea un contenedor para la información adicional
+      var artistDetails = document.createElement('div');
+      artistDetails.innerHTML = '<p class="infoExt">Popularidad: ' + response.popularity + '</p>';
+      artistDetails.innerHTML += '<p class="infoExt">Seguidores: ' + response.followers.total + '</p>';
+      artistDetails.innerHTML += '<p class="infoExt">Géneros: ' + response.genres.join(', ') + '</p>';
+
+      // se agrega todo el contenido del artista (imagen, nombre y detalles) al div info'
+      document.getElementById('artist-info').innerHTML = artistInfo;
+      document.getElementById('artist-info').appendChild(artistDetails);
+
+      // se agrega el contenedor de la información adicional al div details'
+      document.getElementById('artist-details').innerHTML = '';
+      document.getElementById('artist-details').appendChild(artistDetails);
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+  })
+  .catch(function(error) {
+    console.log('Error:', error);
   });
 });
 
-$(document).ready(function() {
+//ALBUMES ------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
   var clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
   var clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
   var artistId = '1Yox196W7bzVNZI7RBaPnf';
 
   // Realiza una solicitud para obtener el token de acceso
-  $.ajax({
-    url: 'https://accounts.spotify.com/api/token',
-    type: 'POST',
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
     headers: {
       'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    data: {
-      'grant_type': 'client_credentials'
-    },
-    success: function(tokenResponse) {
-      var accessToken = tokenResponse.access_token;
-
-      // Realiza la solicitud a la API de Spotify utilizando el token de acceso
-      $.ajax({
-        url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
-        type: 'GET',
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        },
-        success: function(response) {
-          if (response.hasOwnProperty('items')) {
-            var albums = response.items;
-            var albumsContainer = $('#albums-container');
-
-            // Itera sobre los álbumes y muestra la información en el HTML
-            albums.forEach(function(album) {
-              var albumContainer = $('<div class="album-container"></div>');
-              var albumImage = $('<img class="imgAlbum" src="' + album.images[0].url + '">');
-              var albumInfo = $('<div class="album-info"></div>');
-
-              albumInfo.append('<h3>' + album.name + '</h3>');
-              albumInfo.append('<p class="album-info-line">Total de canciones: ' + album.total_tracks + '</p>');
-              albumInfo.append('<p class="album-info-line">Fecha de lanzamiento: ' + album.release_date + '</p>');
-
-              albumContainer.append(albumImage);
-              albumContainer.append(albumInfo);
-
-              albumsContainer.append(albumContainer);
-            });
-          } else {
-            console.log('La respuesta de la API no contiene la propiedad "items".', response);
-          }
-        },
-
-        error: function(error) {
-          console.log('Error:', error);
-        }
-      });
-    },
-    error: function(error) {
-      console.log('Error:', error);
+    body: 'grant_type=client_credentials'
+  })
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error al obtener el token de acceso');
     }
+  })
+  .then(function(tokenResponse) {
+    var accessToken = tokenResponse.access_token;
+
+    // Realiza la solicitud a la API de Spotify utilizando el token de acceso
+    fetch('https://api.spotify.com/v1/artists/' + artistId + '/albums', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error al obtener la información de los álbumes');
+      }
+    })
+    .then(function(response) {
+      if (response.hasOwnProperty('items')) {
+        var albums = response.items;
+        var albumsContainer = document.getElementById('albums-container');
+
+        // Itera sobre los álbumes y muestra la información en el HTML
+        albums.forEach(function(album) {
+          var albumContainer = document.createElement('div');
+          albumContainer.classList.add('album-container');
+          var albumImage = document.createElement('img');
+          albumImage.classList.add('imgAlbum');
+          albumImage.src = album.images[0].url;
+          var albumInfo = document.createElement('div');
+          albumInfo.classList.add('album-info');
+
+          albumInfo.innerHTML += '<h3>' + album.name + '</h3>';
+          albumInfo.innerHTML += '<p class="album-info-line">Total de canciones: ' + album.total_tracks + '</p>';
+          albumInfo.innerHTML += '<p class="album-info-line">Fecha de lanzamiento: ' + album.release_date + '</p>';
+
+          albumContainer.appendChild(albumImage);
+          albumContainer.appendChild(albumInfo);
+
+          albumsContainer.appendChild(albumContainer);
+        });
+      } else {
+        console.log('La respuesta de la API no contiene la propiedad "items".', response);
+      }
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+  })
+  .catch(function(error) {
+    console.log('Error:', error);
   });
 });
 
-// Ejecutar función en el evento click
+
+//ALBUM ESPECIFICO -----------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+  const albumId = '1ziUtOuRT545OI4cnHEMhC';
+  const clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
+  const clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
+
+  // Realiza una solicitud para obtener el token de acceso
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error al obtener el token de acceso');
+      }
+    })
+    .then(function(tokenResponse) {
+      const accessToken = tokenResponse.access_token;
+
+      fetch(`https://api.spotify.com/v1/albums/${albumId}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          const albumImageDiv = document.getElementById('album-image');
+          const albumDetailsDiv = document.getElementById('album-details');
+
+          // Crea elementos HTML para mostrar los datos del álbum
+          const albumImage = document.createElement('img');
+          albumImage.src = data.images[1].url;
+          albumImage.alt = 'Imagen del álbum';
+
+          const albumName = document.createElement('h2');
+          albumName.textContent = '' + data.name;
+
+          const totalTracks = document.createElement('p');
+          totalTracks.textContent = 'Total de canciones: ' + data.total_tracks;
+
+          // Agrega los elementos al div correspondiente
+          albumImageDiv.appendChild(albumImage);
+          albumDetailsDiv.appendChild(albumName);
+          albumDetailsDiv.appendChild(totalTracks);
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+});
+
+
+//TRACKS DEL ALBUM-----------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+  const albumId = '1ziUtOuRT545OI4cnHEMhC';
+  const clientId = 'e44e9944a8dc4ff1aee0224c7e6f2fcc';
+  const clientSecret = '90b9fd64675d4112ad07e4e9f6bfcd8a';
+
+  // Realiza una solicitud para obtener el token de acceso
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error al obtener el token de acceso');
+      }
+    })
+    .then(function(tokenResponse) {
+      const accessToken = tokenResponse.access_token;
+
+      fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Accede al elemento div donde quieres mostrar la información de los tracks
+          const tracksInfoDiv = document.getElementById('tracks-info');
+
+          // Itera sobre cada track y crea elementos HTML para mostrar la información
+          data.items.forEach(track => {
+            const trackInfoContainer = document.createElement('div');
+            trackInfoContainer.classList.add('track-info');
+
+            const trackName = document.createElement('p');
+            trackName.classList.add('track-name');
+            trackName.textContent = ' ' + track.name;
+
+            const trackDuration = document.createElement('p');
+            trackDuration.classList.add('track-duration');
+
+            const minutes = Math.floor(track.duration_ms / 60000);
+            const seconds = ((track.duration_ms % 60000) / 1000).toFixed(0);
+            trackDuration.textContent = 'Duración: ' + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+
+            trackInfoContainer.appendChild(trackName);
+            trackInfoContainer.appendChild(trackDuration);
+            tracksInfoDiv.appendChild(trackInfoContainer);
+          });
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+});
+
+
+
+// Ejecutar función en el evento click-------------------------------------------------------------------
 $('#btn_open').on('click', open_close_menu);
 
 // Declaramos variables
